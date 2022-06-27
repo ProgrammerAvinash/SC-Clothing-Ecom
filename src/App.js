48d84eb77e8f4bb571af08e561d0c6df49cf18a0
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2022-06-27 22:45:36 +0530

    hompage started

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Homepage from './Components/Homepage';
+
+function App(){
+    return(
+        <div>
+            <p>hello</p>
+            <Homepage/>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/Components/Homepage.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Homepage.scss";
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">T-shirts</h1>
+            <span className="sub-title">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">T-shirts</h1>
+            <span className="sub-title">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">T-shirts</h1>
+            <span className="sub-title">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">T-shirts</h1>
+            <span className="sub-title">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">T-shirts</h1>
+            <span className="sub-title">Shop Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
