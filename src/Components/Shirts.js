6435cf95716863e9478a67362a9a062966fee503
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2022-07-03 23:41:26 +0530

    two component addded

---
+++ src/Components/Shirts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Shirts = () => {
+  return <div>Tshirt</div>;
+};
+
+export default Shirts;
---
+++ src/Components/Shorts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Shorts = () => {
+  return <div>Tshirt</div>;
+};
+
+export default Shorts;
