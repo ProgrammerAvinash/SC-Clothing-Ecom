e8ec3c5221dee4416bebf2d412f5db562eb8cf86
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2022-08-01 23:45:51 +0530

    header complete

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { ReactComponent as Logo } from "../../Assets/shopping-logo.svg";
 import { Link } from "react-router-dom";
-
+import "./Header.scss";
 const Header = () => {
   return (
     <div className="header">
       <Link className="logoContainer" to="/">
-        <Logo style={{ height: "50px", width: "20px" }} />
+        <Logo />
       </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          Shop
+        </Link>
+        <Link className="option" to="/contact">
+          Contact
+        </Link>
+      </div>
     </div>
   );
 };
