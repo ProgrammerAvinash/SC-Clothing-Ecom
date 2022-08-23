bbf1be7f2bff245457c8a4cb0df84a8f7b4c1eee
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2022-08-24 00:24:29 +0530

    form-input-label

--- src/Components/Form-Input/Form-Input.js
+++ src/Components/Form-Input/Form-Input.js
@@ -1,4 +1,9 @@
 import React from "react";
 import "./form-input.scss";
 
-const FormInput = (handleChange, label, ...otherProps) => {};
+const FormInput = (handleChange, label, ...otherProps) => (
+  <div>
+    <input className="form-input" onChange={handleChange} {...otherProps} />
+    {label ? <label className={`form-input-label`}></label> : null}
+  </div>
+);
